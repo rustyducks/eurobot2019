d7ccb3631f15447d39939a73219e56f4feaa131f
Fabien Bonneval <fabien.bonneval@gmail.com>
2017-11-16 02:06:40 +0100

    [propulsion] Adds eclipse arduino project.

---
+++ daneel/base/code/propulsion_FAT_2018.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _propulsion_FAT_2018_H_
+#define _propulsion_FAT_2018_H_
+#include "Arduino.h"
+//add your includes for the project propulsion_FAT_2018 here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project propulsion_FAT_2018 here
+
+
+
+
+//Do not add code below this line
+#endif /* _propulsion_FAT_2018_H_ */
