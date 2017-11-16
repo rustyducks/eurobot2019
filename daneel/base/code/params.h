1db4bdaa63a554d48e4b0fc1a681faa8e15e7a9d
Fabien Bonneval <fabien.bonneval@gmail.com>
2017-11-16 02:07:50 +0100

    [propulsion] Adds motor pins.

---
+++ daneel/base/code/params.h
@@ -0,0 +1,33 @@
+/*
+ * params.h
+ *
+ *  Created on: 16 nov. 2017
+ *      Author: fabien
+ */
+
+#ifndef PARAMS_H_
+#define PARAMS_H_
+#include "Arduino.h"
+
+const int MOT1_PWM = 21;
+const int MOT1_DIR = 23;
+const int MOT1_BRK = 22;
+const int MOT1_CURRENT = A6;
+const int MOT1_ENCA = 17;
+const int MOT1_ENCB = 16;
+
+const int MOT2_PWM = 35;
+const int MOT2_DIR = 36;
+const int MOT2_BRK = 39;
+const int MOT2_CURRENT = A22;
+const int MOT2_ENCA = 14;
+const int MOT2_ENCB = 15;
+
+const int MOT3_PWM = 30;
+const int MOT3_DIR = 29;
+const int MOT3_BRK = 28;
+const int MOT3_CURRENT = A21;
+const int MOT3_ENCA = 27;
+const int MOT3_ENCB = 26;
+
+#endif /* PARAMS_H_ */
