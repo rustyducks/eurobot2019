ee1af95fcd32c66ee276c937a5c47574bf1f8440
Fabien-B <fabien.bonneval@gmail.com>
2017-12-13 01:20:03 +0100

    [propulsion] Update main accordingly to previous modifications.

--- daneel/base/code/propulsion_FAT_2018.cpp
+++ daneel/base/code/propulsion_FAT_2018.cpp
@@ -5,6 +5,7 @@
 #include "Metro.h"
 #include "params.h"
 #include "utilities.h"
+#include "ExtNavigation.h"
 //#include "DynamixelSerial5.h"
 
 
@@ -12,11 +13,12 @@
 Metro controlTime = Metro((unsigned long)(CONTROL_PERIOD*1000));
 
 Metro testTime = Metro(4000);
-arm_matrix_instance_f32* testCommands[] = {
-		makeSpeed(0, 0, W_to_RW(600)),
-		makeSpeed(0, 0, W_to_RW(0)),
-		makeSpeed(0, 0, W_to_RW(-600)),
-		makeSpeed(0, 0, W_to_RW(0))
+
+float32_t testCommands[][3] = {
+		{0, 0, W_to_RW(600)},
+		{0, 0, W_to_RW(0)},
+		{0, 0, W_to_RW(-600)},
+		{0, 0, W_to_RW(0)}
 };
 int i = 0;
 
@@ -40,12 +42,12 @@ void loop()
 	if(controlTime.check()) {
 		odometry.update();
 		motorControl.control();
-		//int current = analogRead(MOT1_CURRENT);
-		//Serial.println(current);
+		extNavigation.update();
 	}
 
 	if(testTime.check()) {
-		motorControl.setTargetSpeed(testCommands[i]);
+		//motorControl.setTargetSpeed(testCommands[i][0], testCommands[i][1], testCommands[i][2]);
+		extNavigation.setTableSpeedCons(testCommands[i][0], testCommands[i][1], testCommands[i][2]);
 		i = (i+1) % 4;
 	}
 
