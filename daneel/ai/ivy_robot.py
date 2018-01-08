9d09f7549ad0ac03fb9cc76ff39cde871429b20e
EngelOfChipolata <engelofdeath@live.fr>
2018-01-08 23:55:13 +0100

    [AI] Adds linear go to handeling in slave behavior

--- daneel/ai/ivy_robot.py
+++ daneel/ai/ivy_robot.py
@@ -5,7 +5,8 @@ from ivy.std_api import *
 IVY_APP_NAME = "AI_Robot"
 
 NEW_OBSTACLE_REGEXP = "New Obstacle "
-GO_TO_REGEXP = "Go to (.*)"
+GO_TO_ORIENT_REGEXP = "Go to orient (.*)"
+GO_TO_REGEXP = "Go to linear (.*)"
 NEW_TRAJECTORY_REGEXP = "New trajectory {}"
 UPDATE_ROBOT_POSITION_REGEXP = "Update robot pose {}"
 HIGHLIGHT_POINT_REGEXP = "Highlight point {}"  # Highlight point id;x;y
