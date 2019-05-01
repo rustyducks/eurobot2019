e86be6ec5a64f1c6bcdb8c3f0191239b54d24139
Fabien-B <fabien.bonneval@gmail.com>
2019-05-01 23:28:09 +0200

    Edits teensy flashing script.

--- daneel/base/code/flash_raspi.sh
+++ daneel/base/code/flash_raspi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-scp ./Release/propulsion_rustyDuck_2019.hex pi@digduck:~/teensyBinaries/
+ssh pi@digduck 'rm teensyBinaries/*.hex'
+scp ./Release/*.hex pi@digduck:~/teensyBinaries/
 echo $(pwd)
-ssh pi@digduck 'tools/teensy_loader_cli/teensy_loader_cli -mmcu=mk64fx512 -s -v -w teensyBinaries/propulsion_rustyDuck_2019.hex'
+ssh pi@digduck 'tools/teensy_loader_cli/teensy_loader_cli -mmcu=mk64fx512 -s -v -w teensyBinaries/*.hex'
 
