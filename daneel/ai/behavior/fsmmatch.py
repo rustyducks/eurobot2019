b611242c6afa6137e3a0d6d70d4a7f2939c0ddba
guilhembn <gbuisan@laas.fr>
2019-05-22 20:35:47 +0200

    [AI/FSMMatch] Scores 32 points (goldenium release working ~75%)

--- daneel/ai/behavior/fsmmatch.py
+++ daneel/ai/behavior/fsmmatch.py
@@ -5,6 +5,7 @@ from math import pi
 import os
 import armothy
 from table.table import SlotName, Atom
+from robot_parts import AtomStorage
 
 from behavior import Behavior
 
@@ -86,7 +87,7 @@ class StatePreStartChecks(FSMState):
         if self.robot.io.battery_power_voltage is not None and self.robot.io.battery_signal_voltage is not None:
             if (time.time() - self.enter_time) % 13 < 5:
                 if self.robot.io.battery_signal_voltage <= WARNING_VOLTAGE_THRESHOLD and (
-                    time.time() - self.enter_time) % 2 < 1:
+                        time.time() - self.enter_time) % 2 < 1:
                     self.robot.io.set_led_color(self.robot.io.LedColor.RED)
                 else:
                     self.robot.io.set_led_color(self.robot.io.LedColor.CYAN)
@@ -94,7 +95,7 @@ class StatePreStartChecks(FSMState):
                                                    with_two_points=True)
             elif (time.time() - self.enter_time) % 13 < 10:
                 if self.robot.io.battery_power_voltage <= WARNING_VOLTAGE_THRESHOLD and (
-                    time.time() - self.enter_time) % 2 < 1:
+                        time.time() - self.enter_time) % 2 < 1:
                     self.robot.io.set_led_color(self.robot.io.LedColor.RED)
                 else:
                     self.robot.io.set_led_color(self.robot.io.LedColor.ORANGE)
@@ -213,29 +214,90 @@ class StateGetColorFrontGreenPeriodicAtom(FSMState):
 class StateTakeFirstAtom(FSMState):
     def __init__(self, behavior):
         super().__init__(behavior)
+        self.store_side = None
         if self.behavior.color == Color.YELLOW:
             if self.robot.table.slots[SlotName.YELLOW_PERIODIC_GREEN].atom.color == Atom.Color.RED:
                 #  We put the reds in the right stack, we probably do not want to put them in the scale
-                self.robot.io.armothy.take_and_store(75, armothy.eStack.RIGHT_STACK)
+                self.robot.io.armothy.take_and_store(self.robot.right_storage.armothy_height(), armothy.eStack.RIGHT_STACK)
+                self.store_side = AtomStorage.Side.RIGHT
             else:
                 #  If the atom is green or if we don't know, we put it in the left stack to put it on the scale
-                self.robot.io.armothy.take_and_store(75, armothy.eStack.LEFT_STACK)
+                self.robot.io.armothy.take_and_store(self.robot.left_storage.armothy_height(), armothy.eStack.LEFT_STACK)
+                self.store_side = AtomStorage.Side.LEFT
         else:
             if self.robot.table.slots[SlotName.PURPLE_PERIODIC_GREEN].atom.color == Atom.Color.RED:
-                self.robot.io.armothy.take_and_store(75, armothy.eStack.LEFT_STACK)
+                self.robot.io.armothy.take_and_store(self.robot.left_storage.armothy_height(), armothy.eStack.LEFT_STACK)
+                self.store_side = AtomStorage.Side.LEFT
             else:
-                self.robot.io.armothy.take_and_store(75, armothy.eStack.RIGHT_STACK)
+                self.robot.io.armothy.take_and_store(self.robot.right_storage.armothy_height(), armothy.eStack.RIGHT_STACK)
+                self.store_side = AtomStorage.Side.RIGHT
         self.start_time = time.time()
         self.timeout = 7  # sec
 
     def test(self):
         if self.robot.io.armothy.get_macro_status() == armothy.eMacroStatus.FINISHED:
-            # Probably want to check if it is green if this one was not...
-            return StateTakeSecondAtom
+            self.robot.storages[self.store_side].add(self.robot.table.slots[SlotName.PURPLE_PERIODIC_GREEN].atom)
+            # return StateGoToFrontBluePeriodic
+            return StateGoToFrontBluePeriodic
 
         if time.time() - self.start_time >= self.timeout:
             self.robot.io.armothy.home()
+            # Do not add the atom to the atom tank
+            # return StateGoToFrontBluePeriodic
+            return StateGoToFrontBluePeriodic
+
+    def deinit(self):
+        pass
+
+
+class StateGoToFrontBluePeriodic(FSMState):
+    def __init__(self, behavior):
+        super().__init__(behavior)
+        # TODO(guilhem): Add for purple side as well
+        self.robot.locomotion.follow_trajectory([(500, 1250, -pi / 2), (500, 1145, -pi / 2)])
+
+    def test(self):
+        if self.robot.locomotion.trajectory_finished:
+            if self.robot.table.slots[SlotName.YELLOW_PERIODIC_BLUE].atom.color is not None:
+                # If we already know the atom color
+                return StateTakeSecondAtom
+            else:
+                return StateGetColorFrontBluePeriodicAtom
+
+    def deinit(self):
+        pass
+
+
+class StateGetColorFrontBluePeriodicAtom(FSMState):
+    def __init__(self, behavior):
+        super().__init__(behavior)
+        self.robot.io.armothy.translate_z_axis(5)
+        self.robot.io.armothy.rotate_z_axis(0)
+        self.start_time = time.time()
+        self.time_out = 2  # sec
+
+    def test(self):
+        seen_color = self.robot.io.jevois.uniq_last_puck_color
+        if seen_color is not None:
+            if self.behavior.color == Color.YELLOW:
+                self.robot.table.slots[SlotName.YELLOW_PERIODIC_BLUE].atom.color = Atom.Color(seen_color)
+                if seen_color == "green":
+                    self.robot.table.slots[SlotName.YELLOW_PERIODIC_RED].atom.color = Atom.Color.RED
+                    # TODO(guilhem) Check if the periodic green is still there...
+                    self.robot.table.slots[SlotName.YELLOW_PERIODIC_GREEN].atom.color = Atom.Color.RED
+                return StateTakeSecondAtom
+            else:
+                self.robot.table.slots[SlotName.PURPLE_PERIODIC_BLUE].atom.color = Atom.Color(seen_color)
+                if seen_color == "green":
+                    self.robot.table.slots[SlotName.PURPLE_PERIODIC_RED].atom.color = Atom.Color.RED
+                    # TODO(guilhem) Check if the periodic green is still there...
+                    self.robot.table.slots[SlotName.PURPLE_PERIODIC_GREEN].atom.color = Atom.Color.RED
+                return StateTakeSecondAtom
+        if time.time() - self.start_time >= self.time_out:
             return StateTakeSecondAtom
+
+        def deinit(self):
+            pass
 
     def deinit(self):
         pass
@@ -244,17 +306,84 @@ class StateTakeFirstAtom(FSMState):
 class StateTakeSecondAtom(FSMState):
     def __init__(self, behavior):
         super().__init__(behavior)
-        self.traj_finished = False
-        self.robot.locomotion.follow_trajectory([(500, 1250, -pi / 2), (500, 1155, -pi / 2)])
-        # self.start_time = time.time()
-        # self.robot.locomotion.set_direct_speed(-50, 0, 0)
+        self.store_side = None
+        if self.behavior.color == Color.YELLOW:
+            if self.robot.table.slots[SlotName.YELLOW_PERIODIC_BLUE].atom.color == Atom.Color.RED:
+                #  We put the reds in the right stack, we probably do not want to put them in the scale
+                self.store_side = AtomStorage.Side.RIGHT
+                self.robot.io.armothy.take_and_store(self.robot.right_storage.armothy_height(), armothy.eStack.RIGHT_STACK)
+            else:
+                #  If the atom is green or if we don't know, we put it in the left stack to put it on the scale
+                self.store_side = AtomStorage.Side.LEFT
+                self.robot.io.armothy.take_and_store(self.robot.left_storage.armothy_height(), armothy.eStack.LEFT_STACK)
+        else:
+            if self.robot.table.slots[SlotName.PURPLE_PERIODIC_BLUE].atom.color == Atom.Color.RED:
+                self.store_side = AtomStorage.Side.LEFT
+                self.robot.io.armothy.take_and_store(self.robot.left_storage.armothy_height(), armothy.eStack.LEFT_STACK)
+            else:
+                self.store_side = AtomStorage.Side.RIGHT
+                self.robot.io.armothy.take_and_store(self.robot.right_storage.armothy_height(), armothy.eStack.RIGHT_STACK)
+        self.start_time = time.time()
+        self.timeout = 7  # sec
 
     def test(self):
-        if not self.traj_finished:
-            if self.robot.locomotion.trajectory_finished:
-                self.traj_finished = True
-                self.robot.io.armothy.take_and_store(75, armothy.eStack.LEFT_STACK)
-        if self.traj_finished and self.robot.io.armothy.get_macro_status() == armothy.eMacroStatus.FINISHED:
+        if self.robot.io.armothy.get_macro_status() == armothy.eMacroStatus.FINISHED:
+            self.robot.storages[self.store_side].add(self.robot.table.slots[SlotName.PURPLE_PERIODIC_BLUE].atom)
+            return StateGoToFrontRedPeriodic
+
+        if time.time() - self.start_time >= self.timeout:
+            self.robot.io.armothy.home()
+            # Do not add the atom to the atom tank
+            return StateGoToFrontRedPeriodic
+
+    def deinit(self):
+        pass
+
+
+class StateGoToFrontRedPeriodic(FSMState):
+    def __init__(self, behavior):
+        super().__init__(behavior)
+        # TODO(guilhem): Add for purple side as well
+        self.robot.locomotion.follow_trajectory([(500, 1155, pi / 2), (500, 1345, pi / 2)])
+
+    def test(self):
+        if self.robot.locomotion.trajectory_finished:
+            if self.robot.table.slots[SlotName.YELLOW_PERIODIC_RED].atom.color is not None:
+                # If we already know the atom color
+                return StateTakeThirdAtom
+            else:
+                return StateGetColorFrontRedPeriodicAtom
+
+    def deinit(self):
+        pass
+
+
+class StateGetColorFrontRedPeriodicAtom(FSMState):
+    def __init__(self, behavior):
+        super().__init__(behavior)
+        self.robot.io.armothy.translate_z_axis(5)
+        self.robot.io.armothy.rotate_z_axis(0)
+        self.start_time = time.time()
+        self.time_out = 2  # sec
+
+    def test(self):
+        seen_color = self.robot.io.jevois.uniq_last_puck_color
+        if seen_color is not None:
+            if self.behavior.color == Color.YELLOW:
+                self.robot.table.slots[SlotName.YELLOW_PERIODIC_RED].atom.color = Atom.Color(seen_color)
+                if seen_color == "green":
+                    self.robot.table.slots[SlotName.YELLOW_PERIODIC_BLUE].atom.color = Atom.Color.RED
+                    # TODO(guilhem) Check if the periodic green and blue are still there...
+                    self.robot.table.slots[SlotName.YELLOW_PERIODIC_GREEN].atom.color = Atom.Color.RED
+                return StateTakeThirdAtom
+            else:
+                self.robot.table.slots[SlotName.PURPLE_PERIODIC_RED].atom.color = Atom.Color(seen_color)
+                if seen_color == "green":
+                    self.robot.table.slots[SlotName.PURPLE_PERIODIC_BLUE].atom.color = Atom.Color.RED
+                    # TODO(guilhem) Check if the periodic green and blue are still there...
+                    self.robot.table.slots[SlotName.PURPLE_PERIODIC_GREEN].atom.color = Atom.Color.RED
+                return StateTakeThirdAtom
+        if time.time() - self.start_time >= self.time_out:
             return StateTakeThirdAtom
 
     def deinit(self):
@@ -264,17 +393,34 @@ class StateTakeSecondAtom(FSMState):
 class StateTakeThirdAtom(FSMState):
     def __init__(self, behavior):
         super().__init__(behavior)
-        self.traj_finished = False
-        self.robot.locomotion.follow_trajectory([(500, 1155, pi / 2), (500, 1345, pi / 2)])
-        # self.start_time = time.time()
-        # self.robot.locomotion.set_direct_speed(-50, 0, 0)
+        self.store_side = None
+        if self.behavior.color == Color.YELLOW:
+            if self.robot.table.slots[SlotName.YELLOW_PERIODIC_RED].atom.color == Atom.Color.RED:
+                #  We put the reds in the right stack, we probably do not want to put them in the scale
+                self.store_side = AtomStorage.Side.RIGHT
+                self.robot.io.armothy.take_and_store(self.robot.right_storage.armothy_height(), armothy.eStack.RIGHT_STACK)
+            else:
+                #  If the atom is green or if we don't know, we put it in the left stack to put it on the scale
+                self.store_side = AtomStorage.Side.LEFT
+                self.robot.io.armothy.take_and_store(self.robot.left_storage.armothy_height(), armothy.eStack.LEFT_STACK)
+        else:
+            if self.robot.table.slots[SlotName.PURPLE_PERIODIC_RED].atom.color == Atom.Color.RED:
+                self.store_side = AtomStorage.Side.LEFT
+                self.robot.io.armothy.take_and_store(self.robot.left_storage.armothy_height(), armothy.eStack.LEFT_STACK)
+            else:
+                self.store_side = AtomStorage.Side.RIGHT
+                self.robot.io.armothy.take_and_store(self.robot.right_storage.armothy_height(), armothy.eStack.RIGHT_STACK)
+        self.start_time = time.time()
+        self.timeout = 7  # sec
 
     def test(self):
-        if not self.traj_finished:
-            if self.robot.locomotion.trajectory_finished:
-                self.traj_finished = True
-                self.robot.io.armothy.take_and_store(50, armothy.eStack.LEFT_STACK)
-        if self.traj_finished and self.robot.io.armothy.get_macro_status() == armothy.eMacroStatus.FINISHED:
+        if self.robot.io.armothy.get_macro_status() == armothy.eMacroStatus.FINISHED:
+            self.robot.storages[self.store_side].add( self.robot.table.slots[SlotName.YELLOW_PERIODIC_RED].atom)
+            return StateDropRediums
+
+        if time.time() - self.start_time >= self.timeout:
+            self.robot.io.armothy.home()
+            # Do not add the atom to the atom tank
             return StateDropRediums
 
     def deinit(self):
@@ -282,33 +428,110 @@ class StateTakeThirdAtom(FSMState):
 
 
 class StateDropRediums(FSMState):
+    # TODO Purple
     def __init__(self, behavior):
         super().__init__(behavior)
         self.traj_finished = False
         self.atom1_dropped = False
         self.atom2_droping = False
         self.robot.locomotion.go_to_orient(500, 1550, -pi)
+        self.start_time = time.time()
+        self.drop_timeout = 5
+        self.dropped = 0
 
     def test(self):
         if not self.traj_finished:
             if self.robot.locomotion.trajectory_finished:
                 self.traj_finished = True
-                self.robot.io.armothy.put_down(50, armothy.eStack.LEFT_STACK, 100)
+                if self.behavior.color == Color.YELLOW:
+                    self.robot.io.armothy.put_down(self.robot.right_storage.armothy_height(),
+                                                   armothy.eStack.RIGHT_STACK, 100)
+                    self.start_time = time.time()
         else:
-            if not self.atom1_dropped:
-                if self.robot.io.armothy.get_macro_status() == armothy.eMacroStatus.FINISHED:
-                    self.atom1_dropped = True
-            else:
-                if not self.atom2_droping:
-                    self.robot.io.armothy.put_down(75, armothy.eStack.RIGHT_STACK, -100)
-                    self.atom2_droping = True
-                else:
-                    if self.robot.io.armothy.get_macro_status() == armothy.eMacroStatus.FINISHED:
-                        return StateEnd
+            if self.robot.io.armothy.get_macro_status() == armothy.eMacroStatus.FINISHED:
+                self.dropped += 1
+                if self.behavior.color == Color.YELLOW:
+                    self.robot.right_storage.pop()
+                    if not self.robot.right_storage.is_empty:
+                        self.robot.io.armothy.put_down(self.robot.right_storage.armothy_height(),
+                                                       armothy.eStack.RIGHT_STACK,
+                                                       (-1)**len(self.robot.right_storage.atoms) * 100)
+                        self.start_time = time.time()
+                    else:
+                        return StateDisengageRedPeriodic
+            elif time.time() - self.start_time >= self.drop_timeout:
+                return StateDisengageRedPeriodic
+
+    def deinit(self):
+        self.behavior.score += self.dropped * 6  # 6 pts per Red in Red periodic zone
+
+
+class StateDisengageRedPeriodic(FSMState):
+    def __init__(self, behavior):
+        super().__init__(behavior)
+        self.robot.locomotion.go_straight(-100)
+
+    def test(self):
+        if self.robot.locomotion.relative_command_finished:
+            return StateGoFrontParticleAccelerator
 
     def deinit(self):
         pass
 
+
+class StateGoFrontParticleAccelerator(FSMState):
+    def __init__(self, behavior):
+        super().__init__(behavior)
+        if self.behavior.color == Color.YELLOW:
+            self.robot.locomotion.navigate_to(1710, 1700, math.pi/2)
+            self.robot.io.armothy.rotate_z_axis(200)
+        else:
+            self.robot.locomotion.navigate_to(1290, 1700, math.pi/2)
+            self.robot.io.armothy.rotate_z_axis(-200)
+        self.robot.io.armothy.translate_z_axis(45)
+        self.robot.io.armothy.rotate_y_axis(330)
+
+    def test(self):
+        if self.robot.locomotion.trajectory_finished:
+            return StateEngageParticleAccelerator
+
+    def deinit(self):
+        pass
+
+
+class StateEngageParticleAccelerator(FSMState):
+    def __init__(self, behavior):
+        super().__init__(behavior)
+        self.robot.locomotion.go_straight(50)
+
+    def test(self):
+        if self.robot.locomotion.relative_command_finished:
+            return StateRollParticleAccelerator
+
+
+    def deinit(self):
+        pass
+
+
+class StateRollParticleAccelerator(FSMState):
+    def __init__(self, behavior):
+        super().__init__(behavior)
+        if self.behavior.color == Color.YELLOW:
+            self.robot.io.armothy.rotate_z_axis(-50)
+            self.robot.locomotion.turn(math.radians(7))
+        else:
+            # TODO set z axis for Purple
+            self.robot.io.armothy.rotate_z_axis(200)
+        self.start_time = time.time()
+
+    def test(self):
+        if time.time() - self.start_time >= 1:
+            return StateEnd
+
+    def deinit(self):
+        self.behavior.score += 10  # One atom in the accelerator
+        self.behavior.score += 10  # Goldenium revealed !
+        self.robot.io.score_display_number(self.behavior.score)
 
 #
 # class StatePushGreenPeriodic(FSMState):
