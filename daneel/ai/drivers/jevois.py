3e06dc684d7ccb9ae613d2bc5f2cf9e7c4da7924
guilhembn <gbuisan@laas.fr>
2019-05-28 11:01:47 +0200

    [AI/Jevois] Adds some features

--- daneel/ai/drivers/jevois.py
+++ daneel/ai/drivers/jevois.py
@@ -2,6 +2,7 @@ import serial
 import threading
 import re
 from typing import NamedTuple
+import time
 
 DEFAULT_SERIAL_PATH = "/dev/jevois_serial"
 DEFAULT_BAUDRATE = 115200
@@ -17,6 +18,7 @@ class JeVois:
         self._puck_mutex = threading.Lock()
         self._puck_mutex.acquire()
         self._last_pucks = {"red": [], "green": [], "blue": []}
+        self._read_number = 0
         self._puck_mutex.release()
         self.rt_thread = threading.Thread(target=self._get_pucks)
 #        self._serial.readline()
@@ -34,6 +36,7 @@ class JeVois:
         while True:
             colors = [[], [], []]
             line = self._serial.readline().decode().strip()
+            self._read_number += 1
             for puck_data in line.split(";"):
                 try:
                     x,y,r,mean,col = puck_data.split(" ")
@@ -55,6 +58,13 @@ class JeVois:
         self._puck_mutex.release()
         return tmp
 
+    def get_next_pucks(self):
+        i = self._read_number
+        while self._read_number == i:
+            time.sleep(0.00000001)
+        return self.last_pucks
+
+
     @property
     def uniq_last_puck_color(self):
         len_red = len(self._last_pucks['red'])
@@ -74,21 +84,62 @@ class JeVois:
         closest_sq_distance_to_center = float('inf')
         closest_color = None
         for r in self._last_pucks['red']:
-            d = (r[0] - JeVois.JEVOIS_RES[0]/2)**2 + (r[1] - JeVois.JEVOIS_RES[1]/2)
+            d = (r[0] - JeVois.JEVOIS_RES[0]/2)**2 + (r[1] - JeVois.JEVOIS_RES[1]/2) ** 2
             if d < closest_sq_distance_to_center:
                 closest_sq_distance_to_center = d
                 closest_color = "red"
         for g in self._last_pucks['green']:
-            d = (g[0] - JeVois.JEVOIS_RES[0] / 2) ** 2 + (g[1] - JeVois.JEVOIS_RES[1] / 2)
+            d = (g[0] - JeVois.JEVOIS_RES[0] / 2) ** 2 + (g[1] - JeVois.JEVOIS_RES[1] / 2) ** 2
             if d < closest_sq_distance_to_center:
                 closest_sq_distance_to_center = d
                 closest_color = "green"
         for b in self._last_pucks['blue']:
-            d = (b[0] - JeVois.JEVOIS_RES[0] / 2) ** 2 + (b[1] - JeVois.JEVOIS_RES[1] / 2)
+            d = (b[0] - JeVois.JEVOIS_RES[0] / 2) ** 2 + (b[1] - JeVois.JEVOIS_RES[1] / 2) ** 2
             if d < closest_sq_distance_to_center:
                 closest_sq_distance_to_center = d
                 closest_color = "blue"
         return closest_color
+
+    @property
+    def closest_puck_to_center(self):
+        pucks = self.last_pucks
+        closest_sq_distance_to_center = float('inf')
+        closest_puck = None
+        for r in pucks['red']:
+            d = (r[0] - JeVois.JEVOIS_RES[0]/2)**2 + (r[1] - JeVois.JEVOIS_RES[1]/2)**2
+            if d < closest_sq_distance_to_center:
+                closest_puck = (r[0], r[1], 'red')
+                closest_sq_distance_to_center = d
+        for g in pucks['green']:
+            d = (g[0] - JeVois.JEVOIS_RES[0]/2)**2 + (g[1] - JeVois.JEVOIS_RES[1]/2)**2
+            if d < closest_sq_distance_to_center:
+                closest_puck = (g[0], g[1], 'green')
+                closest_sq_distance_to_center = d
+        for b in pucks['blue']:
+            d = (b[0] - JeVois.JEVOIS_RES[0]/2)**2 + (b[1] - JeVois.JEVOIS_RES[1]/2)**2
+            if d < closest_sq_distance_to_center:
+                closest_puck = (b[0], b[1], 'blue')
+                closest_sq_distance_to_center = d
+        return closest_puck
+
+    @property
+    def closest_puck_to_robot(self):
+        pucks = self.get_next_pucks()
+        min_y = float('inf')
+        closest_puck = None
+        for color, pucks_list in pucks.items():
+            print(color, pucks_list)
+            for p in pucks_list:
+                d = 510 - p[1]
+                if d < min_y:
+                    min_y = d
+                    closest_puck = (p[0], p[1], color)
+        return closest_puck
+
+
+
+
+
 
 
 
